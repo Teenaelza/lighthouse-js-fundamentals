ef2ae2fb66090ec80d596610856b3b164b390622
Teenaelza <teena.thaliath@gmail.com>
2021-07-29 19:08:18 +0000

    added for .. of conditional loop example

--- loops.js
+++ loops.js
@@ -3,3 +3,21 @@ for (var i=100;i<=200;i++)
   
   console.log((i%3===0)&&(i%4===0)?"LoopyLighthouse":i%3===0?"Loopy":i%4===0?"Lighthouse":i);
 }
+const amounts = [61.00, 52.25, 112.99, 5.00]; 
+let total = 0;
+for (let i = 0; i < amounts.length; i++) {
+  total += amounts[i];
+}
+console.log('Order total is: ', total);
+
+let total1 = 0;
+for (let amount of amounts) {
+  total1 += amount;
+}
+console.log('Order total is: ', total1);
+
+let total3 = 0;
+for (let amount of amounts) {
+  total3 += amount;
+}
+console.log('Order total is: ', total3);
